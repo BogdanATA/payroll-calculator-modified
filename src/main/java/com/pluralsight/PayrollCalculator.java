024b21fa71bf5b9e0556cd49c8a512430d741ea3
BogdanATA <batameev@gmail.com>
2026-04-20 15:55:18 -0400

    - created employee class with proper attributes and constructor

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
+
+    private static Employee[] person = new Employee[20];
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
 }
