dbc9d0d8dd4336f3b3e894c681a52f8fa56a4a56
BogdanATA <batameev@gmail.com>
2026-04-21 11:42:03 -0400

    - created user prompts to ask employee file and new file that user wants to create - created buffered writer

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,17 +1,24 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        //get user info
+        System.out.println("Enter the name of the employee file to process: ");
+        String inputFile = scanner.nextLine();
+
+        System.out.println("Enter the name of the payroll file to create: ");
+        String outputFile = scanner.nextLine();
+
         try {
-            FileReader fileReader = new FileReader("employees.csv");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+
             String input; //store each line read from file
 
             while((input = bufferedReader.readLine()) != null) {
@@ -25,8 +32,10 @@ public class PayrollCalculator {
                 //create employee object with the split data we just got
                 Employee employee = new Employee(employeeId, name, hoursWorked, payRate);
 
-                System.out.printf("Employee ID: %d | Name: %s | Gross Pay: $%.2f%n",
-                        employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+                //write employee data into the output file
+                bufferedWriter.write(employee.getEmployeeId() + " |" + employee.getName() + " |" + String.format("%.2f", employee.getGrossPay()));
+                /*System.out.printf("Employee ID: %d | Name: %s | Gross Pay: $%.2f%n",
+                        employee.getEmployeeId(), employee.getName(), employee.getGrossPay());*/
             }
             bufferedReader.close();
         } catch (IOException e) {
